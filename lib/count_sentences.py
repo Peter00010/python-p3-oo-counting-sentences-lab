80c1eb7b00863969ce292fe296815cf88dc3554e
Peter00010 <mwongela.oboe@gmail.com>
2024-03-01 16:01:42 +0300

    Completed lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self,value=""):
+        self.value =value
+
+        if value is not str:
+            print("The value must be a string.")
+        else:
+            self.value = value
+    
+    def is_sentence(self):
+        """Returns True if the value ends with a period, False otherwise"""
+        return self.value.endswith(".")
+    
+    def is_question(self):
+        """Returns True if the value ends with a question mark and False otherwise"""
+        return self.value.endswith("?")
+    
+    def is_exclamation(self):
+        """Returns True if value ends with an exclamation mark and False otherwise"""
+        return self.value.endswith("!")
+    
+    def count_sentences(self):
+        """Returns the number of sentences in the value"""
+        words = self.value.split()
+        sentences = [word for word in words if word.endswith(".") or word.endswith("?") or word.endswith("!")]
+        return len(sentences)
